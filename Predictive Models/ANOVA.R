082f23e32212cf30f98304602363b2238bda88be
CaiEddie <>
2019-07-30 11:26:21 -0400

    Update ANOVA.R

--- Predictive Models/ANOVA.R
+++ Predictive Models/ANOVA.R
@@ -13,7 +13,9 @@ dataCNPC$Time <-rep("NPC",nrow(dataCNPC))
 df <- rbind(rbind(data4weeks, data2weeks), dataCNPC)
 
 
-var_list <- c("Directory","Lines","Time")   # directory is going to tell use the media and passage
+var1_list <- c("Lines")   
+var2_list <- c("Directory", "Media")
+
 features <- c("Ch1.positive",
               "Ch1.positive_per_nuclei",
               "Ch1.intensity",
@@ -30,17 +32,7 @@ set.seed(42)
 
 
 # load libraries
-library(cowplot)
-library(gridExtra)  
-library(ggplot2)
-library(tidyverse)
-library(caret)
-library(leaps)
-library(MASS)
-library(relaimpo)
-library(GGally)
-library(scales)
-library(agricolae)
+
 
 
 
@@ -54,41 +46,18 @@ mean2 = function(x) {
 
 df <- aggregate(df, by = list(df$Well), mean2)
 
-
-
 df$Lines <- factor(df$Column)
 
 data <- df[,colSums(is.na(df))<nrow(df)]
 data <- data[rowSums(is.na(data)) == 0,]
 
 
-for (var1 in var_list) {
-  for (var2 in var_list){
-    if (var1!= var2){
-      for (Feature in features) {
-        
-        
-        variables = paste(Feature, " ~ ", var1, " * ", var2)
-        
-        print(variables)
-        
-        res.aov <- aov(as.formula(variables), data = data)
-        # Summary of the analysis
-        print(summary(res.aov))
-        write.csv(as.matrix(res.aov), file = "ANOVA", na = "")
-        
-        p <- ggplot(data, aes_string(x = var1, y = Feature, fill= var2)) +
-          geom_boxplot()
-        print(p) 
-      }     
-    }
-  }
-  
+for (var1 in var1_list) {
   for (val in unique(data[,var1])){
     
     datatemp <- data[data[,var1]==val,]
     
-    for (var2 in var_list){
+    for (var2 in var2_list){
       if (var1!= var2){
         for (Feature in features) {
           
@@ -96,13 +65,17 @@ for (var1 in var_list) {
           variables = paste(Feature, " ~ ", var2)
           print(val)
           print(variables)
-          res.aov <- aov(as.formula(variables), data = datatemp)    
-          print(summary(res.aov))
-          write.csv(as.matrix(res.aov), file = "ANOVA", na = "")
-          print(TukeyHSD(res.aov))
-          p <- ggplot(datatemp, aes_string(x = var1, y = Feature, fill= var2)) +
-            geom_boxplot()
-          print(p)     
+          if (length(unique(datatemp[,var2])) > 1) {
+            res.aov <- aov(as.formula(variables), data = datatemp)    
+            print(summary(res.aov))
+            write.csv(as.matrix(res.aov), file = "ANOVA", na = "")
+            print(TukeyHSD(res.aov))
+            p <- ggplot(datatemp, aes_string(x = var1, y = Feature, fill= var2)) +
+              geom_boxplot()
+            print(p)
+          } else {
+            print("Less than two factors in vector")
+          }
         }
       }
     }
