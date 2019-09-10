4d94c6898a74fb9604f5f2244d16e0e04615b8c3
CaiEddie <>
2019-09-10 13:44:43 -0400

    updated the .10 size limit

--- fig_4w.R
+++ fig_4w.R
@@ -66,53 +66,40 @@ for (val in c("MAP2", "Tuj","NeuN", "STAB2", "S100B", "NCAM1", "PAX6" )) {
   
   data_temp$Media <- revalue(data_temp$Sample.Name, c("NCRM1"="mTeSR", "KYOU"="mTeSR", "3448"="E8", "3450"="E8", "AJD002-3"="E8", "TD22"="E8", "AIW002-02"="mTeSR", "AJC001-5"="mTeSR", "AJG001-C4"="mTeSR", "TD02"="mTeSR", "TD03" = "E8", "TD10"="E8"))
   
-  
-  plots[[val]] <- ggplot(data_temp, aes(x=Sample.Name, y=Rq.mean, fill=Sample.Name)) +
-    geom_errorbar(aes(ymin=Rq.mean-Rq.SD, ymax=Rq.mean+Rq.SD), width=0.5, position=position_dodge(), size = 1) +
-    geom_bar(stat="identity", position=position_dodge(), width = 1, color = 'black', size = 1) +
-    scale_fill_manual(values = colours)+
-    theme_classic()+
-    theme(axis.text.x = element_blank())+
-    theme(legend.position = "none", axis.title.x = element_blank()) +
-    labs(y = paste("Relative Expression"), title = val) +
-    scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
-    #geom_hline(yintercept=1, linetype="dashed", color = "black")+
-    theme(legend.text=element_text(size=rel(0.8)), legend.key.size= unit(0.8, "cm"))+
-    facet_grid(~Media ,drop = TRUE, scales = "free_x",  space = "free_x", switch = "x")
-  #print(plots[[val]])
+  if (max(data_temp$Rq.mean) < 0.1) {
+    plots[[val]] <- ggplot(data_temp, aes(x=Sample.Name, y=Rq.mean, fill=Sample.Name)) +
+      geom_errorbar(aes(ymin=Rq.mean-Rq.SD, ymax=Rq.mean+Rq.SD), width=0.5, position=position_dodge(), size = 1) +
+      geom_bar(stat="identity", position=position_dodge(), width = 1, color = 'black', size = 1) +
+      scale_fill_manual(values = colours)+
+      theme_classic()+
+      theme(axis.text.x = element_blank())+
+      coord_cartesian(ylim = c(0, 0.1)) +
+      theme(legend.position = "none", axis.title.x = element_blank()) +
+      labs(y = paste("Relative Expression"), title = val) +
+      scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
+      #geom_hline(yintercept=1, linetype="dashed", color = "black")+
+      theme(legend.text=element_text(size=rel(0.8)), legend.key.size= unit(0.8, "cm"))+
+      facet_grid(~Media ,drop = TRUE, scales = "free_x",  space = "free_x", switch = "x")
+    #print(plots[[val]])   
+  } else {
+    plots[[val]] <- ggplot(data_temp, aes(x=Sample.Name, y=Rq.mean, fill=Sample.Name)) +
+      geom_errorbar(aes(ymin=Rq.mean-Rq.SD, ymax=Rq.mean+Rq.SD), width=0.5, position=position_dodge(), size = 1) +
+      geom_bar(stat="identity", position=position_dodge(), width = 1, color = 'black', size = 1) +
+      scale_fill_manual(values = colours)+
+      theme_classic()+
+      theme(axis.text.x = element_blank())+
+      theme(legend.position = "none", axis.title.x = element_blank()) +
+      labs(y = paste("Relative Expression"), title = val) +
+      scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
+      #geom_hline(yintercept=1, linetype="dashed", color = "black")+
+      theme(legend.text=element_text(size=rel(0.8)), legend.key.size= unit(0.8, "cm"))+
+      facet_grid(~Media ,drop = TRUE, scales = "free_x",  space = "free_x", switch = "x")
+    #print(plots[[val]])
+  }
+
 }
 
 
 p <- grid_arrange_shared_legend(plots[[1]],plots[[2]],plots[[3]],plots[[4]],plots[[5]],plots[[6]],plots[[7]],  nrow = 3, ncol = 3, position = "right")
-
-print(grid.draw(p))
-
-plots2 <-list()
-
-for (val in c("SOX1", "SOX9", "NANOG", "OCT3/4", "MIXL1", "AFP")) {
-  data_temp <- data_nu[data_nu[,"Target.Name"]==val,]
-  data_temp <- data_temp %>% group_by(Sample.Name) %>% summarise(Rq.mean = mean(Rq.mean), Rq.SD = mean(Rq.SD))
-  
-  data_temp$Media <- revalue(data_temp$Sample.Name, c("NCRM1"="mTeSR", "KYOU"="mTeSR", "3448"="E8", "3450"="E8", "AJD002-3"="E8", "TD22"="E8", "AIW002-02"="mTeSR", "AJC001-5"="mTeSR", "AJG001-C4"="mTeSR", "TD02"="mTeSR", "TD03" = "E8", "TD10"="E8"))
-  
-  
-  plots2[[val]] <- ggplot(data_temp, aes(x=Sample.Name, y=Rq.mean, fill=Sample.Name)) +
-    geom_errorbar(aes(ymin=Rq.mean-Rq.SD, ymax=Rq.mean+Rq.SD), width=0.5, position=position_dodge(), size = 1) +
-    geom_bar(stat="identity", position=position_dodge(), width = 1, color = 'black', size = 1) +
-    scale_fill_manual(values = colours)+
-    theme_classic()+
-    theme(axis.text.x = element_blank())+
-    theme(legend.position = "none", axis.title.x = element_blank()) +
-    labs(y = paste("Relative Expression"), title = val) +
-    scale_y_continuous(expand = expand_scale(mult = c(0, .1)))+
-    #geom_hline(yintercept=1, linetype="dashed", color = "black")+
-    theme(legend.text=element_text(size=rel(0.8)), legend.key.size= unit(0.8, "cm"))+
-    facet_grid(~Media ,drop = TRUE, scales = "free_x",  space = "free_x", switch = "x") +
-    coord_cartesian(ylim = c(0, 0.1)) 
-  #print(plots[[val]])
-}
-
-
-p <- grid_arrange_shared_legend(plots2[[1]],plots2[[2]],plots2[[3]],plots2[[4]],plots2[[5]],plots2[[6]], nrow = 3, ncol = 2, position = "right")
 
 print(grid.draw(p))
